7d694472e6076c25a6f5ba74d44403d9d740d4bc
Arnau March <arnau@arnaumarch.com>
2015-04-01 11:49:12 +0200

    added bower installation support

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    bump: {
+      // upgrade release and push to master
+      options : {
+        files: ['bower.json'],
+        commitFiles: ["-a"],
+        pushTo: 'origin'
+      }
+    },
+
+    exec: {
+      // add new files before commiting
+      add: {
+        command: 'git add -A'
+      },
+
+      // push to gh-pages branch
+      pages: {
+        command: [
+          'git checkout gh-pages',
+          'git pull origin master',
+          'git push origin gh-pages',
+          'git checkout master'
+        ].join('&&')
+      },
+
+      // adds prompted commit message
+      message: {
+        command: function() {
+          var message = grunt.config('gitmessage');
+          return "git commit -am '" + message + "'";
+        }
+      }
+    },
+
+    prompt: {
+      commit: {
+        options: {
+          questions: [
+            {
+              config: 'gitmessage',
+              type: 'input',
+              message: 'Commit Message'
+            }
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('release', [
+    'exec:add',
+    'prompt',
+    'exec:message',
+    'bump',
+    'exec:pages'
+  ]);
+};
